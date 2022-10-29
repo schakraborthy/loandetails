99c2c5bd86108282824168f00ff0052804ce746d
Saurabh <saurabh.chakraborthy@gmail.com>
2022-10-29 16:50:45 +0530

    First Commit . Initialized first git repository.

---
+++ src/test/java/com/example/loandetails/LoandetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loandetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoandetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
