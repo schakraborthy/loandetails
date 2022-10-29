981faa932760f540db51a0a02f5da27898b95853
Saurabh <saurabh.chakraborthy@gmail.com>
2022-10-29 19:54:46 +0530

    Added logic for making REST calls

--- src/main/java/com/example/loandetails/LoandetailsApplication.java
+++ src/main/java/com/example/loandetails/LoandetailsApplication.java
@@ -2,12 +2,24 @@ package com.example.loandetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class LoandetailsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoandetailsApplication.class, args);
+	}
+
+
+	@Bean
+	public WebClient getWebClient() {
+		return WebClient.builder()
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+				.build();
 	}
 
 }
--- src/main/java/com/example/loandetails/controllers/UserController.java
+++ src/main/java/com/example/loandetails/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.example.loandetails.controllers;
 
 import com.example.loandetails.model.Account;
+import com.example.loandetails.model.AccountDetails;
 import lombok.extern.java.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -15,8 +20,14 @@ public class UserController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
+    private WebClient webClient;
+
+    public UserController(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
     @PostMapping("/account")
-    public String createAccount(@RequestBody Account account) {
+    public Account createAccount(@RequestBody Account account) {
         LOGGER.info("This is great");
 
         LOGGER.info(account.getAccountId());
@@ -25,7 +36,33 @@ public class UserController {
             LOGGER.info(ad.getDetailName());
         });
 
-        return "Done";
+        return account;
     }//end of 'createAccount'
+
+    @GetMapping("/test")
+    public String testAccount(@RequestParam("account_id") String accountId,
+                              @RequestParam("account_detail_id") String accountDetailId) {
+
+        LOGGER.info(accountId);
+        LOGGER.info(accountDetailId);
+
+        AccountDetails ad = new AccountDetails();
+        ad.setDetailId(accountDetailId);
+        ad.setDetailName("Default");
+
+        Account a = new Account();
+        a.setAccountId(accountId);
+        a.setAccountDetails(List.of(ad));
+
+        String s = webClient.post()
+                .uri("http://localhost:8090/api/account")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(Mono.just(a), Account.class)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return s;
+    }//end of 'testAccount'
 
 }//end of class
